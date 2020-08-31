665155e7e4e5c003a7d90d59e827d38e2d49bf32
lonsher91 <pablonnp91@gmail.com>
2020-08-31 10:37:21 -0300

    solucion errores en package.json

---
+++ agenciaViajes.sql
@@ -0,0 +1,29 @@
+Usage: lampp <action>
+
+	start         Start XAMPP (Apache, MySQL and eventually others)
+	startapache   Start only Apache
+	startmysql    Start only MySQL
+	startftp      Start only ProFTPD
+
+	stop          Stop XAMPP (Apache, MySQL and eventually others)
+	stopapache    Stop only Apache
+	stopmysql     Stop only MySQL
+	stopftp       Stop only ProFTPD
+
+	reload        Reload XAMPP (Apache, MySQL and eventually others)
+	reloadapache  Reload only Apache
+	reloadmysql   Reload only MySQL
+	reloadftp     Reload only ProFTPD
+
+	restart       Stop and start XAMPP
+	security      Check XAMPP's security
+
+	enablessl     Enable SSL support for Apache
+	disablessl    Disable SSL support for Apache
+
+	backup        Make backup file of your XAMPP config, log and data files
+
+	oci8          Enable the oci8 extenssion
+
+	panel         Starts graphical XAMPP control panel
+
